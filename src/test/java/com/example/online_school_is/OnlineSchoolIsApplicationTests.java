5575e3531bbaa3cacdf60faf4b07f5ebd531ae6b
Sasha <alexalexbor04@gmail.com>
2024-11-29 21:15:54 +0300

    Initial commit

---
+++ src/test/java/com/example/online_school_is/OnlineSchoolIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_school_is;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchoolIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
